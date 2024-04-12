8117a32f7e356210c5bf52cd1d90b067f4833216
Vadim Kostiuk <kostiukdsfv@gmail.com>
2024-04-12 15:37:59 +0300

    laravel only

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/myproject',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9af20e13ad686b80a98b57b5af0a90033fbcf382',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/myproject' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9af20e13ad686b80a98b57b5af0a90033fbcf382',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
